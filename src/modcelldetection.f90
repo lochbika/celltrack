5d02913e3df70be5252cb788179c5ebc661e2000
Kai Lochbihler <kai.lochbihler@knmi.nl>
2016-08-03 08:47:43 +0000

    check if there are any gridpoints to cluster before iterating all gridpoints in clustering routine

--- src/modcelldetection.f90
+++ src/modcelldetection.f90
@@ -232,73 +232,77 @@ module celldetection
           if(data2d(x,y)>thres .AND. data2d(x,y).ne.missval)mask(x,y)=.true.
         end do
       end do
-    
-      ! assign IDs to continous cells
-      do y=1,ny
-        do x=1,nx
-          neighb=-999
-          if(mask(x,y))then
-            ! gather neighbouring IDs; left,up
-            if(x.ne.1)  neighb(1)=tcl((x-1),y)
-            if(y.ne.1)  neighb(2)=tcl(x,(y-1))
-            ! check if there is NO cluster around the current pixel; create new one
-            if(ALL(neighb==-999))then
-              tcl(x,y)=clID
-              clID=clID+1
-              numIDs=numIDs+1
-            else
-              ! both neighbours are in the same cluster
-              if(neighb(1)==neighb(2).and.neighb(1).ne.-999)then
-                tcl(x,y)=neighb(1)
-              end if
-              ! both neighbors are in different clusters but none of them is (-999)
-              if(neighb(1).ne.neighb(2) .and. neighb(1).ne.-999 .and. neighb(2).ne.-999)then
-                numIDs=numIDs-1
-                tcl(x,y)=MINVAL(neighb)
-                ! update the existing higher cluster with the lowest neighbour
-                do cony=1,ny
-                  do conx=1,nx
-                    if(tcl(conx,cony)==MAXVAL(neighb))tcl(conx,cony)=MINVAL(neighb)
-                  end do
-                end do
-              end if
-              ! both neighbors are in different clusters but ONE of them is empty(-999)
-              if(neighb(1).ne.neighb(2) .and. (neighb(1)==-999 .or. neighb(2)==-999))then
-                tcl(x,y)=MAXVAL(neighb)
-              end if
-            end if
-          end if
-        end do
-      end do
-    
-      ! gather IDs and rename to gapless ascending IDs
-      if(numIDs>0)then
-        allocate(allIDs(numIDs))
-        allIDs=-999
-        clID=startID-1
-        tp=1
+      
+      ! check if there are any gridpoints to cluster
+      if(ANY(mask))then
+      
+        ! assign IDs to continous cells
         do y=1,ny
           do x=1,nx
-            if(.NOT.ANY(allIDs==tcl(x,y)) .AND. tcl(x,y).ne.-999)then
-              allIDs(tp)=tcl(x,y)
-              tp=tp+1
+            neighb=-999
+            if(mask(x,y))then
+              ! gather neighbouring IDs; left,up
+              if(x.ne.1)  neighb(1)=tcl((x-1),y)
+              if(y.ne.1)  neighb(2)=tcl(x,(y-1))
+              ! check if there is NO cluster around the current pixel; create new one
+              if(ALL(neighb==-999))then
+                tcl(x,y)=clID
+                clID=clID+1
+                numIDs=numIDs+1
+              else
+                ! both neighbours are in the same cluster
+                if(neighb(1)==neighb(2).and.neighb(1).ne.-999)then
+                  tcl(x,y)=neighb(1)
+                end if
+                ! both neighbors are in different clusters but none of them is (-999)
+                if(neighb(1).ne.neighb(2) .and. neighb(1).ne.-999 .and. neighb(2).ne.-999)then
+                  numIDs=numIDs-1
+                  tcl(x,y)=MINVAL(neighb)
+                  ! update the existing higher cluster with the lowest neighbour
+                  do cony=1,ny
+                    do conx=1,nx
+                      if(tcl(conx,cony)==MAXVAL(neighb))tcl(conx,cony)=MINVAL(neighb)
+                    end do
+                  end do
+                end if
+                ! both neighbors are in different clusters but ONE of them is empty(-999)
+                if(neighb(1).ne.neighb(2) .and. (neighb(1)==-999 .or. neighb(2)==-999))then
+                  tcl(x,y)=MAXVAL(neighb)
+                end if
+              end if
             end if
           end do
         end do
-    
-        do i=1,tp-1
-          clID=clID+1
+      
+        ! gather IDs and rename to gapless ascending IDs
+        if(numIDs>0)then
+          allocate(allIDs(numIDs))
+          allIDs=-999
+          clID=startID-1
+          tp=1
           do y=1,ny
             do x=1,nx
-              if(tcl(x,y)==allIDs(i))then
-                tcl(x,y)=clID
+              if(.NOT.ANY(allIDs==tcl(x,y)) .AND. tcl(x,y).ne.-999)then
+                allIDs(tp)=tcl(x,y)
+                tp=tp+1
               end if
             end do
           end do
-        end do
-        deallocate(allIDs)
+      
+          do i=1,tp-1
+            clID=clID+1
+            do y=1,ny
+              do x=1,nx
+                if(tcl(x,y)==allIDs(i))then
+                  tcl(x,y)=clID
+                end if
+              end do
+            end do
+          end do
+          deallocate(allIDs)
+        end if
+        
       end if
-    
       ! return final cluster ID
       finID=clID
     end subroutine clustering
