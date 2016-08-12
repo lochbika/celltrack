3d9bb3bca782bc13e0f41960e7da9d45593a7f1c
Kai Lochbihler <kai.lochbihler@knmi.nl>
2016-08-12 12:46:53 +0000

    added variable ID for sample size in vfield output

--- src/modncdfio.f90
+++ src/modncdfio.f90
@@ -18,7 +18,7 @@ module ncdfpars
   ! Variables for netCDF actions
   integer :: gridID1,taxisID1,vlistID1,varID1,streamID1,tsID,zaxisID1
   integer :: gridID2,taxisID2,vlistID2,varID2,streamID2,zaxisID2,vuID,vvID
-  integer :: gridID3,taxisID3,vlistID3,varID3,streamID3,zaxisID3
+  integer :: gridID3,taxisID3,vlistID3,varID3,streamID3,zaxisID3,ssizeID
   integer :: nmiss1,nmiss2,nmiss3
   real(kind=8) :: inmissval,outmissval
   character(len=1000) :: vunit,xunit,yunit,vname
