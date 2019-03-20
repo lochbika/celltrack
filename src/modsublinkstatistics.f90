d8a9f9b1cf3ba7a35a3ce9070ec286c307348402
Kai Lochbihler <kai_lochi@knmi.nl>
2019-03-20 16:21:07 +0000

    added explanation of current step to be written to STDout

--- src/modsublinkstatistics.f90
+++ src/modsublinkstatistics.f90
@@ -1,10 +1,10 @@
 !-------------------------------------------------------------------------------------------
-!  ######  ######## ##       ##       ######## ########     ###     ######  ##    ## 
-! ##    ## ##       ##       ##          ##    ##     ##   ## ##   ##    ## ##   ##  
-! ##       ##       ##       ##          ##    ##     ##  ##   ##  ##       ##  ##   
-! ##       ######   ##       ##          ##    ########  ##     ## ##       #####    
-! ##       ##       ##       ##          ##    ##   ##   ######### ##       ##  ##   
-! ##    ## ##       ##       ##          ##    ##    ##  ##     ## ##    ## ##   ##  
+!  ######  ######## ##       ##       ######## ########     ###     ######  ##    ##
+! ##    ## ##       ##       ##          ##    ##     ##   ## ##   ##    ## ##   ##
+! ##       ##       ##       ##          ##    ##     ##  ##   ##  ##       ##  ##
+! ##       ######   ##       ##          ##    ########  ##     ## ##       #####
+! ##       ##       ##       ##          ##    ##   ##   ######### ##       ##  ##
+! ##    ## ##       ##       ##          ##    ##    ##  ##     ## ##    ## ##   ##
 !  ######  ######## ######## ########    ##    ##     ## ##     ##  ######  ##    ##
 !-------------------------------------------------------------------------------------------
 ! This file is part of celltrack
@@ -13,34 +13,38 @@
 ! Any help will be appreciated :)
 !
 module sublinkstatistics
-  
+
   use globvar
 
   implicit none
-  
+
   contains
     subroutine calcsublinkstatistics
-      
+
       use globvar
-     
+
       implicit none
-      
+
       integer :: cltp(globnIDs)             ! counter of subcells for each cell
 
       write(*,*)"======================================="
       write(*,*)"========= SUBLINK STATISTICS =========="
       write(*,*)"======================================="
-    
+
       write(*,*)"======================================="
       write(*,*)"=== calc number of sublinks per cell ..."
       write(*,*)"---------"
-    
+
       ! now we can find out how many subcells a cell has
       allocate(clIDnsub(globnIDs))
       clIDnsub=0
       do i=1,globsubnIDs
         clIDnsub(sublinks(i))=clIDnsub(sublinks(i))+1
       end do
+
+      write(*,*)"======================================="
+      write(*,*)"=== find subcells for each cell ..."
+      write(*,*)"---------"
 
       ! which subcells belong to a cell
       allocate(clIDsub(globnIDs,MAXVAL(clIDnsub)))
@@ -54,7 +58,11 @@ module sublinkstatistics
           end if
         end do
       end do
-
+      
+      write(*,*)"======================================="
+      write(*,*)"=== writing links to sublinks.txt ..."
+      write(*,*)"---------"
+      
       ! write the links to file
       open(unit=1,file="sublinks.txt",action="write",status="replace")
       write(1,*)"   subclID       clID"
@@ -63,6 +71,10 @@ module sublinkstatistics
       end do
       close(unit=1)
 
+      write(*,*)"======================================="
+      write(*,*)"=== writing detailed subcell stats for each cell to cells_subcell_stats.txt ..."
+      write(*,*)"---------"
+      
       ! write detailed subcell statistics for each cell, similar to meta track statistics
       open(unit=1,file="cells_subcell_stats.txt",action="write",status="replace")
       do i=1,globnIDs
@@ -78,11 +90,11 @@ module sublinkstatistics
           & subtouchb(clIDsub(i,k)),subcldate(clIDsub(i,k)),subcltime(clIDsub(i,k))
         end do
       end do
-      
+
       write(*,*)"======================================="
       write(*,*)"===== FINISHED SUBLINK STATISTICS ====="
       write(*,*)"======================================="
-    
+
     end subroutine calcsublinkstatistics
-    
+
 end module sublinkstatistics
